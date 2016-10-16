7dffdd0a05c08549028dec53347eed966d4c09c8
fb920024 <francesco.benedetto263@gmail.com>
2016-10-16 12:50:48 +0200

    Login page added

---
+++ src/pages/login/login.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the Login page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html'
+})
+export class Login {
+  accessCodesList: string[];
+
+  constructor(public navCtrl: NavController) {
+    this.accessCodesList = ['ac1', 'ac2', 'ac3'];
+  }
+
+  ionViewDidLoad() {
+    console.log('Hello Login Page');
+  }
+
+  login(accessCode: string) {
+
+  }
+
+  modifyAccessCode(code: string, index: number) {
+
+  }
+
+}
