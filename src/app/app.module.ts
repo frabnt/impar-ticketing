de38f24c857ea7c524fedaf9e96d40a2e6a190bc
fb920024 <francesco.benedetto263@gmail.com>
2016-10-16 13:58:59 +0200

    About and contact pages removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import {LoginPage} from "../pages/login/login";
@@ -12,8 +10,6 @@ import {LogoComponent} from "../pages/logo-component/logo-component";
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
     TabsPage,
     LoginPage,
@@ -26,8 +22,6 @@ import {LogoComponent} from "../pages/logo-component/logo-component";
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
     TabsPage,
     LoginPage,
