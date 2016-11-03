01f5714bc55dc08221051557add4147e8013fee7
fb920024 <francesco.benedetto263@gmail.com>
2016-11-03 17:21:29 +0100

    Added methods to vfs-api-service to get manifest and tickets data

--- src/providers/vfs-api-service.ts
+++ src/providers/vfs-api-service.ts
@@ -12,6 +12,8 @@ import 'rxjs/add/operator/toPromise';
 @Injectable()
 export class VfsApiService {
   private authBaseUrl: string = 'https://vfs.staging.vendini.com/api/v1/auth/registration';
+  private manifestBaseURL: string = 'https://vfs.staging.vendini.com/api/v1/scanning/sync';
+  private ticketsBaseUrl: string = `https://vfs.staging.vendini.com/api/v1/scanning/tickets?`;
   private apiToken: string;
   private eventID: string;
 
@@ -22,11 +24,12 @@ export class VfsApiService {
   constructor(public http: Http) { }
 
   /**
-   * Set api credentials used for authentication (login and logout)
-   * @param apiToken
-   * @param eventID
+   * Set API token and event ID returned by the server after a successful authentication.
+   * These credentials are sent in all subsequent http requests from client to server
+   * @param {string} apiToken - authentication token
+   * @param {string} eventID - ID of the event
    */
-  setCredentials(apiToken, eventID): void {
+  setCredentials(apiToken: string, eventID: string): void {
     this.apiToken = apiToken;
     this.eventID = eventID;
   }
@@ -71,8 +74,40 @@ export class VfsApiService {
   }
 
   /**
+   * Generate http headers used to retrieve manifest
+   * @returns {Headers}
+   */
+  private generateManifestHeaders() {
+    let headers = new Headers;
+    headers.append('Content-Type',
+      'application/json');
+    headers.append('X-VENDINI-DEVICE-ID',
+      '22229C46-8813-4494-B654-BCCA4C366CB1');
+    headers.append('X-VENDINI-API-TOKEN', this.apiToken);
+    headers.append('X-VENDINI-EVENT-ID', this.eventID);
+
+    return headers;
+  }
+
+  /**
+   * Generate http headers used to retrieve tickets
+   * @returns {Headers}
+   */
+  private generateTicketsHeaders(): Headers {
+    let headers = new Headers;
+    headers.append('Content-Type',
+      'application/json');
+    headers.append('X-VENDINI-DEVICE-ID',
+      '22229C46-8813-4494-B654-BCCA4C366CB1');
+    headers.append('X-VENDINI-API-TOKEN', this.apiToken);
+    headers.append('X-VENDINI-EVENT-ID', this.eventID);
+
+    return headers;
+  }
+
+  /**
    * Perform login http (POST) request
-   * @param accessCode
+   * @param {string} accessCode - the access code of the event
    * @returns {Promise<Response>}
    */
   doLogin(accessCode: string): Promise<any> {
@@ -106,6 +141,40 @@ export class VfsApiService {
         headers: headers
       }
     ).toPromise();
+  }
+
+  /**
+   * Perform an http (GET) request to retrieve manifest data
+   * @returns {Promise<Response>}
+   */
+  getManifest(): Promise<any> {
+    let headers = this.generateManifestHeaders();
+
+    return this.http.get(
+      this.manifestBaseURL,
+      {
+        headers: headers
+      }
+    ).toPromise();
+  }
+
+  /**
+   * Perform an http (GET) request to retrieve tickets data
+   * @param {number} page - page to retrieve
+   * @param {number} items - number of page items to retrieve
+   * @returns {Promise<Response>}
+   */
+  getTickets(page: number, items?: number): Promise<any> {
+    let url: string = this.ticketsBaseUrl;
+    if(items) {
+      // url updated if items parameter is provided
+      url += `items=${items}&`;
+    }
+    url += `page=${page}`;
+
+    return this.http.get(url, {
+      headers: this.generateTicketsHeaders()
+    }).toPromise();
   }
 
 }
